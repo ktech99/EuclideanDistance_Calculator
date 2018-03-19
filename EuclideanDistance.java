f0b82fce1c39c704369c8dcd746b9ac158fd3302
Kartik Arora <33497301+ktech99@users.noreply.github.com>
2018-03-18 21:46:54 -0700

    Added user Interaction

--- EuclideanDistance.java
+++ EuclideanDistance.java
@@ -3,22 +3,37 @@ import java.util.*;
 public class EuclideanDistance {
 
   public static void main(String[] args) {
-    Map<String, List<Integer>> rating = new TreeMap<String, List<Integer>>();
-    List<Integer> stars = new ArrayList<Integer>();
-    stars.add(5);
-    stars.add(4);
-    rating.put("john", stars);
-    double johnMovie1 = rating.get("john").get(0);
-    double johnMovie2 = rating.get("john").get(1);
-    stars.clear();
-    stars.add(4);
-    stars.add(1);
-    rating.put("jack", stars);
-    double jackMovie1 = rating.get("jack").get(0);
-    double jackMovie2 = rating.get("jack").get(1);
+    Map<String, List<Double>> rating = new TreeMap<String, List<Double>>();
+    Scanner sc = new Scanner(System.in);
+    Scanner sc2 = new Scanner(System.in);
+    System.out.println("How many people do you want to have?");
+    int people = sc2.nextInt();
+    for (int i = 0; i < people; i++) {
+      List<Double> stars = new ArrayList<Double>();
+      System.out.println("Enter critic " + (i + 1) + "'s name :");
+      String name = sc.nextLine();
+      System.out.println("enter rating 1 :");
+      stars.add(sc2.nextDouble());
+      System.out.println("enter rating 2 :");
+      stars.add(sc2.nextDouble());
+      rating.put(name, stars);
+    }
+    System.out.println("Enter the first name to compare:");
+    String name1 = sc.nextLine();
+    System.out.println("Enter the second name to compare:");
+    String name2 = sc.nextLine();
+    if (!(rating.containsKey(name1) || rating.containsKey(name2))) {
+      throw new IllegalArgumentException();
+    }
+    double person1Movie1 = rating.get(name1).get(0);
+    double person1Movie2 = rating.get(name1).get(1);
+    double person2Movie1 = rating.get(name2).get(0);
+    double person2Movie2 = rating.get(name2).get(1);
     double distance =
-        Math.sqrt(Math.pow(johnMovie1 - jackMovie1, 2) + Math.pow(johnMovie2 - jackMovie2, 2));
+        Math.sqrt(
+            Math.pow(person1Movie1 - person2Movie1, 2)
+                + Math.pow(person1Movie2 - person2Movie2, 2));
     double distInverse = 1 / (1 + distance);
-    System.out.println(distInverse);
+    System.out.println("The score is" + distInverse);
   }
 }
